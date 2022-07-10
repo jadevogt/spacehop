62960eed0449960b604be31f28f40c08a52895e4
jadevogt <me@jadev.dev>
2022-07-09 21:49:33 -0400

    initial commit for django tests

--- tests/test_spacehop.py
+++ tests/test_spacehop.py
@@ -1,3 +1,6 @@
+"""
+Tests for spacehop
+"""
 from django.test import TestCase
 from spacehop.characters.models import Character, Expression
 from spacehop.events.models import Event, TextBox, FullscreenTextBox, ChangeCharacters, ChangeAudio, Delay
@@ -6,17 +9,11 @@ from spacehop.sounds.models import AudioTrack, SoundEffect
 from spacehop.images import *
 
 class CharacterTestCase(TestCase):
-	def setUp(self):
+	"""
+	Test Character models
+	"""
+	def setUp(self) -> None:
 		Character.objects.create(name="Test Character")
 
-class AnimalTestCase(TestCase):
-    def setUp(self):
-        Animal.objects.create(name="lion", sound="roar")
-        Animal.objects.create(name="cat", sound="meow")
-
-    def test_animals_can_speak(self):
-        """Animals that can speak are correctly identified"""
-        lion = Animal.objects.get(name="lion")
-        cat = Animal.objects.get(name="cat")
-        self.assertEqual(lion.speak(), 'The lion says "roar"')
-        self.assertEqual(cat.speak(), 'The cat says "meow"')
+	def run_some_other_test(self) -> None:
+		return
